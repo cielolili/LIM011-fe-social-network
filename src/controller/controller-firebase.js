a0837de58fd5065c647ad9f5a55a693817402805
cielolili <cielito.torres@hotmail.com>
2019-12-12 16:38:00 -0500

    subir cambios

--- src/controller/controller-firebase.js
+++ src/controller/controller-firebase.js
@@ -1,14 +1,12 @@
-// eslint-disable-next-line import/prefer-default-export
-// eslint-disable-next-line max-len
 export const signIn = (email, password) => firebase.auth().createUserWithEmailAndPassword(email, password);
-// eslint-disable-next-line max-len
+  // const errorCode = error.code;
 export const logIn = (email, password) => firebase.auth().signInWithEmailAndPassword(email, password);
 
 export const googleLogin = () => {
   let provider = new firebase.auth.GoogleAuthProvider();
   return firebase.auth().signInWithPopup(provider);
-};
+} 
 export const facebookLogin = () => {
   let provider = new firebase.auth.FacebookAuthProvider();
   return firebase.auth().signInWithPopup(provider);
-};
+} 
